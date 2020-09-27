669f12b172e14f717267c8d5bc229ed1773e6989
cosyer <chenyu@mydearest.cn>
2020-09-28 00:01:31 +0800

    gh pages public path

--- app/config/index.js
+++ app/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-introduction/',
     proxyTable: {},
 
     // Various Dev Server settings
